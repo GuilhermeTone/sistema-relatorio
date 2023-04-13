04dd35db8376babbc1db80e8c5fca07a6fd5d33c
Guilherme Tonelotti <guilhermetonelotti33@gmail.com>
2023-04-12 22:39:08 -0300

    alterando labels

--- resources/views/lojas/index.blade.php
+++ resources/views/lojas/index.blade.php
@@ -10,7 +10,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Cadastrar Produtos') }}
+            {{ __('Lojas') }}
         </h2>
     </x-slot>
 
--- resources/views/produtos/editar.blade.php
+++ resources/views/produtos/editar.blade.php
@@ -9,7 +9,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Cadastrar Produtos') }}
+            {{ __('Editar Produtos') }}
         </h2>
     </x-slot>
 
