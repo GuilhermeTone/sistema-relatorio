108f722733c1dae7703d4905e7e8dfc3bcee3e5b
Guilherme Tonelotti <guilhermetonelotti33@gmail.com>
2023-04-26 20:42:07 -0300

    Arrumando menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,7 +27,7 @@
                     </x-nav-link> --}}
                     @if(Auth::user()->Cargo == 'admin')
 
-                    <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out font-medium rounded-lg text-sm px-1 pt-1 pb-3 text-center inline-flex items-center" type="button">Menu<svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                    <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out font-medium rounded-lg text-sm px-1 pt-1 pb-1.5 text-center inline-flex items-center" type="button">Menu<svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
                     <!-- Dropdown menu -->
                     <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
