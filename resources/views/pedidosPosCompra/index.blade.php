0109d5496c586728739335a1b52f7c74da46d807
Guilherme Tonelotti <guilhermetonelotti33@gmail.com>
2023-05-13 15:57:14 -0300

    Corrigindo erro confirmado e tirando possibilidade de aparecer a loja 1 pos compra

--- resources/views/pedidosPosCompra/index.blade.php
+++ resources/views/pedidosPosCompra/index.blade.php
@@ -48,9 +48,9 @@
                         <select id="loja" name ="loja" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option selected disabled>Selecione A loja</option>
                             @foreach ($lojas as $loja)
-                                {{-- @if($loja->idLoja != 1) --}}
+                                @if($loja->idLoja != 1)
                                     <option value="{{$loja->idLoja}}">{{$loja->Nome}}</option>
-                                {{-- @endif --}}
+                                @endif
                             @endforeach
                         </select>
                 </div>
